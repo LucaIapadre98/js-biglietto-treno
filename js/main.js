eaa010907d30260f60ac0c6756125cf00c76e8e7
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-01 16:33:36 +0200

    svolgimento con if

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
- // # Raccolta Dati 
-const firstUserAge = parseInt(prompt("Età del passeggero:"));
-const distance = parseInt(prompt("Distanza in km"));
+// # Raccolta Dati 
+const firstUserAge = parseInt(prompt("Età del passeggero"));
+const distance = parseInt(prompt("Numero dei km")); // distanza in km
 const priceAtKm = 0.21;     // prezzo al km
-const price = distance * priceAtKm;     // prezzo totale del viaggio
-const discountPerc1 = 20;   // scotnto per i minorenni
+const totalprice = distance * priceAtKm;     // prezzo totale del viaggio
+
+const discountPerc1 = 20;   // sconto per i minorenni
 const discountPerc2 = 40;  // sconto per gli over.65
-const discountAmount = price * discountPerc1 / 100; // sconto per i minorenni
-const discountAmount2 = price * discountPerc2 / 100; // sconto per gli over.65
+
+const discountAmount1 = totalprice * discountPerc1 / 100; // sconto per i minorenni
+const discountAmount2 = totalprice * discountPerc2 / 100; // sconto per gli over.65
 
 console.log(firstUserAge);
 console.log(distance);
+console.log(discountAmount1);
+console.log(discountAmount2);
+console.log(priceAtKm);
 
 // # SVOLGIMENTO
-if (conditions) {
+if (firstUserAge < 18 ) {
+        console.log ( discountAmount1 );
+    } else if (firstUserAge > 65) {
+        console.log ( discountAmount2 );
+    }
     
-}
+
+
+
+
 // OUTPUT
 
 
