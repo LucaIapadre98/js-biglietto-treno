722d861127586a6de59b7cdba9e3366376166165
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-01 17:41:18 +0200

    Controllo generale

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // # Raccolta Dati 
-const firstUserAge = parseInt(prompt("Età del passeggero"));
-const distance = parseInt(prompt("Numero dei km")); // distanza in km
-const priceAtKm = 0.21;     // prezzo al km
+const firstUserAge = parseInt(prompt("Età del passeggero")); // età del passeggero
+const distance = parseInt(prompt("Numero dei km"));   // distanza in km
+const priceAtKm = 0.21 ;     // prezzo al km
 const totalprice = distance * priceAtKm;     // prezzo totale del viaggio
 
 const discountPerc1 = 20;   // sconto per i minorenni
@@ -17,24 +17,24 @@ console.log(discountAmount2);
 console.log(priceAtKm);
 
 
-// # SVOLGIMENTO
+
+// # SVOLGIMENTO 
 if (firstUserAge > 18 && firstUserAge < 65) {
-        console.log(totalprice);
+        console.log(totalprice + "€");
+        
     }
     else if (firstUserAge < 18) {
-        console.log(totalprice - discountAmount1);
-        alert("Il prezzo del biglietto con sconto è di " + (totalprice - discountAmount1).toFixed(0.21) + "€");
+        console.log(totalprice + discountAmount1 + "€");
     }
     else if (firstUserAge > 65) {
-        console.log(totalprice - discountAmount2);
-        alert("Il prezzo del biglietto con sconto è di " + (totalprice - discountAmount2) .toFixed(0.21) + "€");
+        console.log(totalprice + discountAmount2 + "€");
     }
     
 
 
 // # OUTPUT
-output = alert("Il prezzo del biglietto è di " + totalprice.toFixed(0.21) + "€");
-output = alert("Il prezzo del biglietto con sconto è di " + (totalprice - discountAmount1).toFixed(0.21) + "€");
-output = alert("Il prezzo del biglietto con sconto è di " + (totalprice - discountAmount2).toFixed(0.21) + "€");
+  output = alert( "Il prezzo del biglietto è di " + totalprice.toFixed(0.21) + "€");
+  output = alert("Il prezzo del biglietto con sconto minorenni è di " + (totalprice - discountAmount1) .toFixed(0.21) + "€");
+  output = alert("Il prezzo del biglietto con sconto over65 è di " + (totalprice - discountAmount2) .toFixed(0.21) + "€"); 
 
 
