e8342a4a5ba42b9c35cac958d9c42153ed12e33d
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-01 15:46:32 +0200

    svolgimento

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-alert ("questo messaggio è per gli utenti");
+ // # Raccolta Dati 
+const firstUserAge = parseInt(prompt("Età del passeggero:"));
+const distance = parseInt(prompt("Distanza in km"));
+const priceAtKm = 0.21;     // prezzo al km
+const price = distance * priceAtKm;     // prezzo totale del viaggio
+const discountPerc1 = 20;   // scotnto per i minorenni
+const discountPerc2 = 40;  // sconto per gli over.65
+const discountAmount = price * discountPerc1 / 100; // sconto per i minorenni
+const discountAmount2 = price * discountPerc2 / 100; // sconto per gli over.65
 
-console.log("questo messaggio è per i developer");
+console.log(firstUserAge);
+console.log(distance);
+
+// # SVOLGIMENTO
+if (conditions) {
+    
+}
+// OUTPUT
+
+
